0490afe517ffd0c4076d8b89c14651fbd1c9dfda
istay <68560774+IStayThatWayCoding@users.noreply.github.com>
2023-05-29 13:29:39 -0400

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -50,9 +50,9 @@ export default function Projects() {
                                             Aquasis is a Discord Bot built for a content creator, "SwiftBlade"/"Oasis." It currently serves as replacing miscellaneous tasks! The github repository can be found by clicking the button below!
                                         </p>
                                         <p className="text-purple-300 py- text-justify">
-                                            "I can't recommend "istay" enough! They did an incredible job developing and coding a Discord server bot for me. Seriously, their skills are top-notch. The bot they created is just amazing and it works to my needs. istay is not only talented but also super friendly and easy to work with. I'm beyond happy with the results and would definitely choose them again for any future projects :D" <br/>- Oasis/Swiftblade (5/29/23)
+                                            "I can't recommend "istay" enough! They did an incredible job developing and coding a Discord server bot for me. Seriously, their skills are top-notch. The bot they created is just amazing and it works to my needs. istay is not only talented but also super friendly and easy to work with. I'm beyond happy with the results and would definitely choose them again for any future projects :D" <br/><a href="https://www.google.com">- Oasis/Swiftblade (5/29/23)</a>
                                         </p>
-                                        <p><a href="https://www.google.com" style="color: #FF00FF">Test</a></p>
+        
                                         <div className="flex space-x-2">
                                             <a className="rounded-full" href="https://www.github.com/istaythatwaycoding/aquasis" target="_blank">
                                                 <svg className="h-8 w-8" fill="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
