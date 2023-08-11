217e5bc476cadc626d75d87f4202a61be21ffefb
istay <68560774+IStayThatWayCoding@users.noreply.github.com>
2023-08-10 22:42:05 -0400

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -72,9 +72,9 @@ export default function Projects() {
                                     <br>
                                     </br>
                                     <div className="flex-grow space-y-2">
-                                        <h1 className="text-3xl font-bold">Aquasis</h1>
+                                        <h1 className="text-3xl font-bold">Dream Hosting</h1>
                                         <p className="text-gray-400 py- text-justify">
-                                            Aquasis is a Discord Bot built for a content creator, "SwiftBlade"/"Oasis." It currently serves as replacing miscellaneous tasks! The github repository can be found by clicking the button below!
+                                            Dream Hosting is a fairly new server hosting company, owned by a good friend of IStay. You can check out everything on the discord server (by clicking the link button below)
                                         </p>
                                         <p className="text-purple-300 py- text-justify">
                                             "I can't recommend "istay" enough! They did an incredible job developing and coding a Discord server bot for me. Seriously, their skills are top-notch. The bot they created is just amazing and it works to my needs. istay is not only talented but also super friendly and easy to work with. I'm beyond happy with the results and would definitely choose them again for any future projects :D" <br/><a href="https://discord.gg/RtsZcajWCR" target="_blank">- Oasis/Swiftblade (5/29/23)</a>
@@ -90,10 +90,20 @@ export default function Projects() {
                                                 </svg>
                                             </a>
                                         </div>
+                                        <div className="flex space-x-2">
+                                            <a className="rounded-full" href="https://www.github.com/istaythatwaycoding/aquasis" target="_blank">
+                                                <svg className="h-8 w-8" fill="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                    <title>Link</title>
+                                                    <path d="M14.8284 12L16.2426 13.4142L19.071 10.5858C20.6331 9.02365 20.6331 6.49099 19.071 4.9289C17.509 3.3668 14.9763 3.3668 13.4142 4.9289L10.5858 7.75732L12 9.17154L14.8284 6.34311C15.6095 5.56206 16.8758 5.56206 17.6568 6.34311C18.4379 7.12416 18.4379 8.39049 17.6568 9.17154L14.8284 12Z" />
+                                                    <path d="M12 14.8285L13.4142 16.2427L10.5858 19.0711C9.02372 20.6332 6.49106 20.6332 4.92896 19.0711C3.36686 17.509 3.36686 14.9764 4.92896 13.4143L7.75739 10.5858L9.1716 12L6.34317 14.8285C5.56212 15.6095 5.56212 16.8758 6.34317 17.6569C7.12422 18.4379 8.39055 18.4379 9.1716 17.6569L12 14.8285Z" />
+                                                    <path d="M14.8285 10.5857C15.219 10.1952 15.219 9.56199 14.8285 9.17147C14.4379 8.78094 13.8048 8.78094 13.4142 9.17147L9.1716 13.4141C8.78107 13.8046 8.78107 14.4378 9.1716 14.8283C9.56212 15.2188 10.1953 15.2188 10.5858 14.8283L14.8285 10.5857Z" />
+                                                </svg>
+                                            </a>
+                                        </div>
                                     </div>
                                     <br>
                                     </br>
-                                    
+
                                     <div className="flex-grow space-y-2">
                                         <h1 className="text-3xl font-bold">IStay's Utilities Bot</h1>
                                         <p className="text-gray-400 py- text-justify">
